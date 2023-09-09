a17c1d3be6a26897e4b0c45c38ae430d85a8316b
J.C <132265058+weIlThought@users.noreply.github.com>
2023-09-09 09:39:25 +0200

    Added files

---
+++ fxmanifest.lua
@@ -0,0 +1,45 @@
+files {
+    'index.html',
+    'music.mp3',
+    'css/fontawesome-all.min.css',
+    'css/plugins.min.css',
+    'css/style-city.min.css',
+    'css/css.css',
+    'css/style.min.css',
+    'img/logo.png',
+    'img/background/slide2-bg.jpg',
+    'img/background/slide3-bg.jpg',
+    'js/plugins-savage.js',
+    'js/plugins.js',
+    'js/savage.min.js',
+    'js/yt.js',
+    'webfonts/fa-brands-400.eot',
+    'webfonts/fa-brands-400.svg',
+    'webfonts/fa-brands-400.ttf',
+    'webfonts/fa-brands-400.woff',
+    'webfonts/fa-brands-400.woff2',
+    'webfonts/fa-light-300.eot',
+    'webfonts/fa-light-300.svg',
+    'webfonts/fa-light-300.ttf',
+    'webfonts/fa-light-300.woff',
+    'webfonts/fa-light-300.woff2',
+    'webfonts/fa-regular-400.eot',
+    'webfonts/fa-regular-400.svg',
+    'webfonts/fa-regular-400.ttf',
+    'webfonts/fa-regular-400.woff',
+    'webfonts/fa-regular-400.woff2',
+    'webfonts/fa-solid-900.eot',
+    'webfonts/fa-solid-900.svg',
+    'webfonts/fa-solid-900.ttf',
+    'webfonts/fa-solid-900.woff',
+    'webfonts/fa-solid-900.woff2',
+}
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+
+game 'gta5'
+
+
+-- Open source code, by Striikez Development team, ps enjoy :)
